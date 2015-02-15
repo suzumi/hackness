5771794ca7c6cbe6e52457e458d07239cebb92e0
k-shibue <shibue@blue-corporation.jp>
2015-02-16 02:42:06 +0900

    スキーマの追加、初期データの追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150211163512) do
+ActiveRecord::Schema.define(version: 20150215135939) do
+
+  create_table "article_tags", force: :cascade do |t|
+    t.integer  "article_id"
+    t.integer  "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "articles", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +32,12 @@ ActiveRecord::Schema.define(version: 20150211163512) do
     t.string   "name"
     t.text     "url"
     t.text     "feed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Tag.create(
+    [
+        {name: 'PHP'},
+        {name: 'Scala'},
+        {name: 'Rails'},
+        {name: 'HTML'},
+        {name: 'Play framework'},
+        {name: 'Vagrant'},
+        {name: 'Swift'},
+        {name: 'CentOS'},
+        {name: 'Objective-C'},
+        {name: 'CSS'},
+    ]
+)
+
+Blog.create(
+    [
+        {name: 'Ytake Blog.', url: 'http://blog.comnect.jp.net/', feed: 'http://blog.comnect.jp.net/feed'},
+    ]
+)
+
+Article.create(
+    [
+        {name: 'laravel-elixirにタスク追加する方法(タブ -> スペース変換など)', url: 'http://blog.comnect.jp.net/blog/120', blog_id: '1'},
+        {name: 'PhpStormでLaravel5快適開発　おさらい', url: 'http://blog.comnect.jp.net/blog/119', blog_id: '1'},
+    ]
+)
+
+ArticleTag.create(
+    [
+        {article_id: 1, tag_id: 1},
+        {article_id: 1, tag_id: 2},
+        {article_id: 1, tag_id: 3},
+        {article_id: 1, tag_id: 4},
+        {article_id: 1, tag_id: 5},
+    ]
+)
