8698a73f318b4494a81f4512e7f7f576f53665b5
渋江 一晃 <shibue_kazuaki_gn@cyberagent.co.jp>
2015-02-16 12:49:18 +0900

    vendor配下に移動

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module Hackness
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.paths << "#{Rails}/vender/assets/fonts"
   end
 end
