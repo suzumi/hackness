e6199c8d04fe5bccd1a8910d4b58160b8d7fac0e
k-shibue <shibue@blue-corporation.jp>
2015-03-07 04:13:36 +0900

    ブログページをタイムライン表示に変更

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,12 +1,16 @@
-$( document ).ready(function() {
-    //$("[rel='tooltip']").tooltip();
+$(document).ready(function(){
+    var my_posts = $("[rel=tooltip]");
 
-    $('.thumbnail').hover(
-        function(){
-            $(this).find('.caption').slideDown(250).fadeIn(250);
-        },
-        function(){
-            $(this).find('.caption').slideUp(250).fadeOut(205);
+    var size = $(window).width();
+    for(i=0;i<my_posts.length;i++){
+        the_post = $(my_posts[i]);
+
+        if(the_post.hasClass('invert') && size >=767 ){
+            the_post.tooltip({ placement: 'left'});
+            the_post.css("cursor","pointer");
+        }else{
+            the_post.tooltip({ placement: 'rigth'});
+            the_post.css("cursor","pointer");
         }
-    );
+    }
 });
